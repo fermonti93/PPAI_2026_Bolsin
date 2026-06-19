ab4d139cab17fcc896a8061610f4163eca8e3b45
fer <fer.monti@hotmail.com.ar>
2026-06-18 21:06:15 -0300

    primer commit  inicial

---
+++ src/main/java/Grup_7/PPAI_2026_Bolsin/Ppai2026BolsinApplication.java
@@ -0,0 +1,13 @@
+package Grup_7.PPAI_2026_Bolsin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppai2026BolsinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ppai2026BolsinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Grup_7/PPAI_2026_Bolsin/Ppai2026BolsinApplicationTests.java
@@ -0,0 +1,13 @@
+package Grup_7.PPAI_2026_Bolsin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppai2026BolsinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
