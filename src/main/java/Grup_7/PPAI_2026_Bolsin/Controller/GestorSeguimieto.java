ef410f5a72b915077d5faf57e7b68d75b5493045
fer <fer.monti@hotmail.com.ar>
2026-06-26 19:22:25 -0300

    base de la clase controler

---
+++ src/main/java/Grup_7/PPAI_2026_Bolsin/Controller/GestorSeguimieto.java
@@ -0,0 +1,36 @@
+package Grup_7.PPAI_2026_Bolsin.Controller;
+
+import java.time.LocalDateTime;
+
+import Grup_7.PPAI_2026_Bolsin.model.Bolsin;
+import Grup_7.PPAI_2026_Bolsin.model.ComisionMedica;
+import Grup_7.PPAI_2026_Bolsin.model.Usuario;
+
+public interface GestorSeguimieto {
+
+        public Usuario nombreCMUsuarioLogeado;
+        public ComisionMedica cMOrigen;
+        public Bolsin bolsinesEnviados;
+        public int numeroPrecinto;
+        public int numeroBolsin;
+        public ComisionMedica cMDestino;
+        public int posicionBolsin; //revisar tipo de variable
+        public Bolsin bolsinseleccionado;
+        public LocalDateTime fechaHoraActual;
+
+        public void crearConsultaSeguimiento(){}
+        public void buscarNombreCMUsuarioOrigen(){}
+        public void buscarBolsinesEnviados(){}
+        public void obtenerDatosLocalizacionBolsin(){}
+        public void obtenerMapa(){}
+        public void tomarSeleccionBolsin(){}
+        public void enviarEmail(){}
+        public void tomarConfirmacionEmail(){}
+        public void buscarDireccionCorreo(){}
+        public void getFerchaHoraActual(){}
+        public void llamarCUNotificarUbicacionDeBolsin(){}
+        public void finCU(){}
+
+
+
+}
